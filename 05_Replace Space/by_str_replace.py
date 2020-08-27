c6d44a2b61e493c640c90458ee9cea965a15f0bc
Brian Chiang <brianchiang1988@icloud.com>
2020-08-27 15:23:09 +0800

    Add 05: Replace Space in String

---
+++ 05_Replace Space/by_str_replace.py
@@ -0,0 +1,37 @@
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+
+        return s.replace(' ', '%20')
+
+
+# n : the character length of s
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the cost of iteration on string, which is of O( n )
+
+## Space Comeplxtiy: O( n )
+#
+# The overhead in space is the storage for string output, which is of O( n ).
+
+
+import unittest
+
+class Testing( unittest.TestCase ):
+
+    def test_case_1( self ):
+
+        result = Solution().replaceSpace(s="We are happy.")
+        self.assertEqual(result, "We%20are%20happy.")
+
+
+    def test_case_2( self ):
+
+        result = Solution().replaceSpace(s="How are you?")
+        self.assertEqual(result, "How%20are%20you?")
+
+
+
+if __name__ == '__main__':
+
+    unittest.main()
