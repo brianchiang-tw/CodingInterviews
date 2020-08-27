0c4529ae616db3f2b8b4bb0c21d6dd604be700f6
Brian Chiang <brianchiang1988@icloud.com>
2020-08-27 14:26:15 +0800

    Add complexity analysis to 03_find Duplicate In Array

--- 03_Find Duplicate In Array/by_linear_scan.py
+++ 03_Find Duplicate In Array/by_linear_scan.py
@@ -32,6 +32,18 @@ class Solution(object):
                 seen_zero = True
 
 
+
+# n :　the length of input list, nums
+
+## Time Complexiy: O( n )
+#
+# The overhead in time is the cost of for-loop iteration, which is of O( n )
+
+## Space Complexity: O( 1 )
+#
+# The overhead in space is the storage for loop index and temporary variable, which is of O( 1 )
+
+
 import unittest
 
 class Testing( unittest.TestCase ):
